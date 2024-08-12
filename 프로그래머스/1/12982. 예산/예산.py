c045c22d54971ce28f435b996a40fc3395a7ca05
Joonheui <iuehnoojmik@naver.com>
2024-08-12 20:40:48 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    d.sort()
+    count = 0
+    
+    for i in d:
+        if budget >= i:
+            budget -= i
+            count += 1
+        else:
+            break
+
+    return count
