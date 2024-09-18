c104544fc42ac151af7a607c9833c782322e6467
Joonheui <iuehnoojmik@naver.com>
2024-09-19 03:05:39 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,15 @@
+def divisor():
+    n, k = map(int, input().split())
+
+    count = 0
+
+    for i in range(1, n+1):
+        if n%i == 0:
+            count += 1
+            if count == k:
+                print(i)
+                return
+    
+    print(0)
+
+divisor()
